d46f6aae6bf320e17601e1c0e8f4e7c869771f72
feliperichardsontjs-coder <feliperichardsontjs@gmail.com>
2026-08-09 00:49:22 -0300

    Create Home component in Page.js

---
+++ Page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-6">
+      <h1 className="text-5xl font-bold mb-2 text-blue-500">HIBRIDX</h1>
+      <p className="text-gray-400 mb-8">SEJA O VETERANO</p>
+      <button className="w-full max-w-sm p-3 rounded-lg bg-blue-500 font-bold">ENTRAR PARA O BETA</button>
+    </main>
+  )
+}
